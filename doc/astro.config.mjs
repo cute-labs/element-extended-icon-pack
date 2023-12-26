c29e8c62de16d3c493627424a0a817f757cd7f03
richhost <hexcy@pm.me>
2023-12-26 13:12:19 +0800

    📄 doc: react package

---
+++ doc/astro.config.mjs
@@ -0,0 +1,31 @@
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [
+    starlight({
+      title: "Element UI 扩展图标库",
+      social: {
+        github: "https://github.com/withastro/starlight"
+      },
+      sidebar: [
+        {
+          label: "Guides",
+          items: [
+            // Each item here is one entry in the navigation menu.
+            { label: "Example Guide", link: "/guides/example/" }
+          ]
+        },
+        {
+          label: "Reference",
+          autogenerate: { directory: "reference" }
+        },
+        {
+          label: "包",
+          autogenerate: { directory: "packages" }
+        }
+      ]
+    })
+  ]
+});
