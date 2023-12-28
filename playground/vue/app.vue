764cf2a005917dc52c8cad1224e103cbaf4559e8
richhost <hexcy@pm.me>
2023-12-28 13:56:16 +0800

    😜 playground: Vue

---
+++ playground/vue/app.vue
@@ -0,0 +1,31 @@
+<template>
+  <FillEleArrowCaretDouble size="48" />
+
+  <div class="list">
+    <component
+      :is="Icon"
+      v-for="(Icon, key) in icons"
+      :key="key"
+      :size="28"
+      color="oklch(72.65% 0.2 46.59)"
+    />
+  </div>
+
+  <div>
+    <NuxtWelcome />
+  </div>
+</template>
+
+<script setup>
+import { FillEleArrowCaretDouble } from "@element-extended-icon-pack/vue";
+import * as icons from "@element-extended-icon-pack/vue";
+</script>
+
+<style>
+.list {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  gap: 0.2rem;
+}
+</style>
