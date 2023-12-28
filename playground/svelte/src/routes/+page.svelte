ce4a0e4cf68947b1fbec296229d2e3821a82f7cd
richhost <hexcy@pm.me>
2023-12-28 13:39:39 +0800

    😜 playground: Svelte

---
+++ playground/svelte/src/routes/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import { FillEleArrowCaretDouble } from '@element-extended-icon-pack/svelte';
+	import * as icons from '@element-extended-icon-pack/svelte';
+</script>
+
+<FillEleArrowCaretDouble size={20} color="red" />
+
+<div>
+	{#each Object.entries(icons) as [_, icon]}
+		<svelte:component this={icon} color="blue" size="16" />
+	{/each}
+</div>
+
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
