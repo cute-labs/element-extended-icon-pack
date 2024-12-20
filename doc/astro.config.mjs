ee3ca098adbbd2bc8936593c3650b86ba6583061
richhost <hexcy@pm.me>
2024-12-20 13:15:16 +0800

    update doc

--- doc/astro.config.mjs
+++ doc/astro.config.mjs
@@ -1,29 +1,27 @@
-import { defineConfig } from "astro/config";
-import starlight from "@astrojs/starlight";
+// @ts-check
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
 
-import svelte from "@astrojs/svelte";
+import svelte from '@astrojs/svelte';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    starlight({
-      title: "Element UI 扩展图标库",
-      social: {
-        github: "https://github.com/richhost/element-extended-icon-pack"
-      },
-      sidebar: [
-        {
-          label: "Icons",
-          link: "/icons"
+    integrations: [starlight({
+        title: 'Element UI 扩展图标库',
+        social: {
+            github: 'https://github.com/richhost/element-extended-icon-pack',
         },
-        {
-          label: "包",
-          autogenerate: {
-            directory: "packages"
-          }
-        }
-      ]
-    }),
-    svelte()
-  ]
+        sidebar: [
+            {
+                label: "Icons",
+                link: "/icons"
+            },
+            {
+                label: "包",
+                autogenerate: {
+                    directory: "packages"
+                }
+            }
+        ],
+    }), svelte()],
 });
